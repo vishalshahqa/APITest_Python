6dcea370039741493df30c1ec87ba09ff091b507
Vishal Shah <vishalshah.qa@gmail.com>
2022-08-08 17:26:56 -0400

    Removed earlier files

--- scripts/getcomments.py
+++
@@ -1,5 +0,0 @@
-from util.restAPI import *
-
-url = "https://jsonplaceholder.typicode.com/comments"
-
-# get_API(url,200)
--- scripts/verifyGetPost.py
+++
@@ -1,6 +0,0 @@
-
-# from util.restAPI import *
-
-# # Get Call for existing posts
-# url = "https://jsonplaceholder.typicode.com/posts"
-# get_API(url,200)
